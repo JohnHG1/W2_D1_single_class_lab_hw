4f77e4058b6c9ff28dc8b236a12720dbcb93be90
JohnHG1 <honisz@yahoo.com>
2020-01-21 00:25:12 +0000

    Final2

--- code_clan_team.rb
+++ code_clan_team.rb
@@ -13,35 +13,35 @@ class CodeClan_Sports_Team
     return @coach_name
   end
 
-  def number_of_players(players)
-       return player_name @player_name.count
-  end
-
-  def add_input_player_name(new_player)
-   new_player[:players_name].push(new_player)
-  end
-
-  def check_team_name(team_name)
-    return @team_name
-  end
-
-  def check_player_name(player_name)
-    for player in @player_name
-      if player == player_name
-        return player_name
-      end
-    end
-    return nil
-  end
-
-  def find_player_by_name(player_name)
-      for player in team[:player_name]
-        return name if name == player_name[:players_name]
-      end
-        return nil
-    end
-
-
-
+  # def number_of_players(players)
+  #      return player_name @player_name.count
+  # end
+  #
+  # def add_input_player_name(new_player)
+  #  new_player[:players_name].push(new_player)
+  # end
+  #
+  # def check_team_name(team_name)
+  #   return @team_name
+  # end
+  #
+  # def check_player_name(player_name)
+  #   for player in @player_name
+  #     if player == player_name
+  #       return player_name
+  #     end
+  #   end
+  #   return nil
+  # end
+  #
+  # def find_player_by_name(player_name)
+  #     for player in team[:player_name]
+  #       return name if name == player_name[:players_name]
+  #     end
+  #       return nil
+  #   end
+  #
+  #
+  #
 
 end
